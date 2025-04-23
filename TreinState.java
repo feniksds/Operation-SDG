642ae4533cdf21cff2d7d9684d761f078cacbeed
Feniks <feniksdesmet@yahoo.com>
2025-04-23 17:07:22 +0200

    TRANSPORT  af

--- TreinState.java
+++ TreinState.java
@@ -1,25 +1,29 @@
 import java.util.Map;
-//TODO stats veranderingen aanvullen
+
 public class TreinState extends State {
     public TreinState() {
         super("Hoe verplaats je je van thuis naar het station \nof van campus naar je kot?", Map.of(
             1, "Fiets",
             2, "Bus",
             3, "Te voet",
-            4, "E-step"
+            4, "Tram"
         ));
     }
 
     @Override
     public State verwerkKeuze(int keuze, StudentStats stats) {
         if (keuze == 1) {
-             //actie stats
+             stats.financieleImpact += Main.monthToWeekly(11);
         } else if (keuze == 2) {
-            //actie stats
+            //bus
+            stats.co2Uitstoot +=0.106*3.9;
+            stats.financieleImpact += Main.yeartoWeekly(215);
         }else if(keuze == 3){
-            //actie stats
-        }else if(keuze == 4){
-            //actie stats
+            //niks
+        }else if(keuze == 4) {
+            //metro
+            stats.co2Uitstoot += 0.041*3.9;
+            stats.financieleImpact += Main.yeartoWeekly(215);
         }
         return new EtenStartState();
     }
